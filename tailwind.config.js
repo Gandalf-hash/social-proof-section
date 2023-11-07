f3134cbe058f186143778b82b2c40c2de48f9b46
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-07 15:59:53 +0200

    proj-setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "primary-color": "#202A44",
+      "secondary-color": "#808080",
+      "text-color": "#ffffff",
+      "border-bg": "#f2b294",
+    },
+  },
+  plugins: [],
+};
