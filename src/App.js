8948ceb8fb0f4a7ff1d6c5dcc45ff59289f4695e
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-09 10:27:41 +0200

    feat: enhance responsive design

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Ratings from "./ui/Ratings";
 function App() {
   return (
     <div className="flex text-center md:text-start flex-col w-full max-w-full p-6 xl:px-20 xl:py-28">
-      <div className="flex md:flex-row flex-col justify-between">
-        <div className="w-full flex flex-col gap-10 max-w-sm lg:max-w-md">
-          <h1 className="font-bold text-primary-color lg:text-6xl text-4xl">
+      <div className="flex md:flex-row flex-col justify-center xl:justify-between">
+        <div className="w-full flex flex-col gap-10 mb-14 max-w-sm lg:max-w-md">
+          <h1 className="font-bold text-primary-color lg:text-6xl text-5xl">
             10,000+ of our users love our products.
           </h1>
-          <p className="md:text-xl tracking-tighter  text-primary-color">
+          <p className="text-xl tracking-tighter text-primary-color">
             We only provide great products combined with excellent customer
             service. See what our satisfied customers are saying about our
             services.
           </p>
         </div>
-        <div className="xl:mr-40 xl:mb-20 flex justify-start  items-start lg:mt-12">
+        <div className="xl:mr-40 xl:mb-20 flex justify-start place-items-start lg:mt-12">
           <Ratings />
         </div>
       </div>
--- src/ui/Ratings.jsx
+++ src/ui/Ratings.jsx
@@ -17,24 +17,24 @@ function Ratings() {
   ];
 
   return (
-    <div className="w-full">
-      <div className="flex flex-col gap-4 max-w-xl">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={`flex flex-row w-full justify-center max-w-xl lg:gap-2 pr-6 border-none items-center rounded-xl bg-cream py-4 ${
-              index === 1 ? "xl:ml-16" : index === 2 ? "xl:ml-32" : ""
-            }`}
-          >
+    <div className="flex w-full flex-col gap-4 max-w-xl">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className={`flex w-full lg:flex-row xl:justify-center justify-start max-w-xl gap-2 xl:pr-6 border-none items-center rounded-xl bg-cream py-4 ${
+            index === 1 ? "md:ml-16" : index === 2 ? "md:ml-32" : ""
+          } ${index >= 0 ? "flex-col" : "flex-row"}`}
+        >
+          <div className="flex flex-row gap-2">
             {[...Array(5)].map((_, i) => (
               <img key={i} src={item.icon} alt="" />
             ))}
-            <span className="lg:text-xl text-sm ml-4 lg:ml-6 font-bold text-primary-color">
-              {item.text}
-            </span>
           </div>
-        ))}
-      </div>
+          <span className="lg:text-xl text-sm lg:ml-6 font-bold text-primary-color">
+            {item.text}
+          </span>
+        </div>
+      ))}
     </div>
   );
 }
